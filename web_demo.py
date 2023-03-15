80d8381769f772014226afeb4a1de93a2793356e
AdamBear <adamxiong@126.com>
2023-03-15 16:48:17 +0800

    update

--- web_demo.py
+++ web_demo.py
@@ -3,7 +3,7 @@ import gradio as gr
 
 tokenizer = AutoTokenizer.from_pretrained("/data/chatglm-6b", trust_remote_code=True)
 #model = AutoModel.from_pretrained("/data/chatglm-6b", trust_remote_code=True).half().cuda()
-model = AutoModel.from_pretrained("/data/chatglm-6b", trust_remote_code=True).cuda()
+model = AutoModel.from_pretrained("/data/chatglm-6b", trust_remote_code=True).half().cuda()
 model = model.eval()
 
 MAX_TURNS = 20
